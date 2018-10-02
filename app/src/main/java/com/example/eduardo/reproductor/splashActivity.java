a3e0b1a2570793043670e65f6182df361b8b56fe
carolina rodriguez <carolina.rodriguez0597@hotmail.com>
2018-10-01 22:01:51 -0700

    splash

---
+++ app/src/main/java/com/example/eduardo/reproductor/splashActivity.java
@@ -0,0 +1,33 @@
+package com.example.eduardo.reproductor;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+    private Boolean botonBackPresionado=false;
+    private static  final int DURACION_SPLASF=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler manejador = new Handler();
+        manejador.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                if (!botonBackPresionado){
+                    Intent intento = new Intent(splashActivity.this,Explorador.class);
+                    startActivity(intento);
+                }
+            }
+
+        }, DURACION_SPLASF);
+    }
+
+    public void OnBackPressed(){
+        botonBackPresionado=true;
+        super.onBackPressed();
+    }
+}
